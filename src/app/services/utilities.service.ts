c9154a813c0ac0eb5121cd245396f87dca14e468
wilson.gonzalez <wilson.gonzalez.ext@itau.co>
2023-04-20 14:44:21 -0500

    Enabled the multi language and logistic module

--- src/app/services/utilities.service.ts
+++ src/app/services/utilities.service.ts
@@ -4,7 +4,72 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class UtilitiesService {
-
+  private _language= [
+    {
+      login: {
+        email: 'CORREO ELECTRONICO',
+        password: 'CLAVE',
+        remember: 'RECORDAR CORREO',
+        btnLogin: 'INGRESAR'
+      },
+      sideBar: {
+        panel: 'PANEL DE ADMINISTRACION',
+        setting: 'CONFIGURACION',
+        store: 'ALMACEN',
+        logistic: 'LOGISTICA',
+        goOut: 'SALIR'
+      },
+      setting: {
+        title: 'PANEL DE ADMINISTRATION',
+        email: 'CORREO ELECTRONICO',
+        language: 'IDIOMA',
+        btnSave: 'GUARDAR CONFIGURACION'
+      },
+      store: {
+        title: 'PRODUCTO',
+        store: 'BODEGA',
+        manufacture: 'FABRICANTE',
+        importer: 'IMPORTADOR',
+        count: 'CANTIDAD',
+        titleDetail: 'DETALLE DEL PRODUCTO',
+        category: 'CATEGORIA',
+        expired: 'CADUCIDAD',
+        located: 'LOCALIZATION'
+      },
+    },
+    {
+      login: {
+        email: 'EMAIL',
+        password: 'PASSWORD',
+        remember: 'REMEMBER EMAIL',
+        btnLogin: 'LOGIN'
+      },
+      sideBar: {
+        panel: 'ADMINISTRATION',
+        setting: 'SETTING',
+        store: 'STORE',
+        logistic: 'LOGISTIC',
+        goOut: 'GO UT'
+      },
+      setting: {
+        title: 'ADMINISTRATION',
+        email: 'EMAIL',
+        language: 'LANGUAGE',
+        btnSave: 'SAVE CHANGES'
+      },
+      store: {
+        title: 'PRODUCT',
+        store: 'STORE',
+        manufacture: 'MANUFACTURE',
+        importer: 'IMPORTER',
+        count: 'COUNT',
+        titleDetail: 'DETAIL OF PRODUCT',
+        category: 'CATEGORY',
+        expired: 'CADUCIDAD',
+        located: 'LOCALIZATION'
+      },
+    }
+  ];
   constructor() {
   }
 
@@ -116,5 +181,16 @@ export class UtilitiesService {
       endIndex,
       pages
     };
+  }
+
+  getLanguage(){
+    const language = localStorage.getItem('LANGUAGE') ? localStorage.getItem('LANGUAGE') : 'ESPAÑOL' ;
+    if(language === 'ESPAÑOL'){
+       return this._language[0];
+    }
+
+    if(language === 'INGLES'){
+      return this._language[1];
+    }
   }
 }
